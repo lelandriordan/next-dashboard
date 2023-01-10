752453b9de5ec38454fb612289d640231f9e4eea
Leland Riordan <leland@eastrocklabs.com>
2023-01-10 12:38:28 -0500

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,28 @@
+export default function Head() {
+  return (
+    <>
+      <title>Next.js Dashboard By Leland Riordan</title>
+      {/* Primary Meta Tags */}
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="title" content="Next.js Dashboard By Leland Riordan" />
+      <meta name="description" content="Next.js Dashboard by Leland Riordan is a starter project for making dashboard applications with Next, Tailwind and Typescript." />
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://github.com/lelandriordan/next-dashboard" />
+      <meta property="og:title" content="Next.js Dashboard By Leland Riordan" />
+      <meta property="og:description" content="Next.js Dashboard by Leland Riordan is a starter project for making dashboard applications with Next, Tailwind and Typescript." />
+      <meta property="og:image" content="/example.jpg" />
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://github.com/lelandriordan/next-dashboard" />
+      <meta property="twitter:title" content="Next.js Dashboard By Leland Riordan" />
+      <meta property="twitter:description" content="Next.js Dashboard by Leland Riordan is a starter project for making dashboard applications with Next, Tailwind and Typescript." />
+      <meta property="twitter:image" content="/example.jpg" />
+    </>
+  )
+}
