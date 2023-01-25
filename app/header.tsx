dbc0050a79ce35e30a4256a5586b8ee6eb7bdf87
Leland Riordan <leland@eastrocklabs.com>
2023-01-25 10:20:55 -0500

    New Header / Nav

--- app/header.tsx
+++ app/header.tsx
@@ -1,18 +1,36 @@
-import Link from "next/link"
-import GlobalNav from "../components/global-nav"
+import clsx from 'clsx'
+import Link from 'next/link'
+import GlobalNav from './global-nav'
+import { Menu, X } from 'react-feather'
+import ColorModeToggle from '../components/color-mode-toggle'
 
 export interface HeaderProps {
   className?: string
 }
 
-export default function Header({className}: HeaderProps) {
-  const baseClasses = 'flex flex-col items-start gap-4 shadow-md p-2'
-  const classes = className ? `${baseClasses} ${className}` : baseClasses
+const Header = ({className}: HeaderProps) => {
+
+  const classes = {
+    base: 'flex items-center h-auto justify-between gap-4 bg-white shadow-md',
+    responsive: 'md:justify-start md:flex-col md:items-start md:py-2 md:rounded-r-lg',
+    colorMode: 'dark:bg-slate-800'
+  }
 
   return (
-    <div className={classes}>
-      <Link href='/' className="mx-auto text-xl py-2 px-4">Next Dashboard</Link>
-      <GlobalNav />
+    // <div className={classes}>
+    <div className={clsx(
+      classes.base,
+      classes.responsive,
+      classes.colorMode,
+      className && className
+    )}>
+      <Link href="/" className="block mx-2 text-xl rounded-full p-4 bg-indigo-900 text-indigo-100">ND</Link>
+      
+      <GlobalNav className="hidden md:block" />
+
+      <ColorModeToggle className="self-center mt-auto mb-4" />
     </div>
   )
 }
+
+export default Header
